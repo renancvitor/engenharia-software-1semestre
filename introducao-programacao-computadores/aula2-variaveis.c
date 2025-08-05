fa2f06858b3f4c07018ff81eeb93628e0d6018a5
renancvitor <renan.vitor.cm@gmail.com>
2025-08-05 15:12:02 -0300

    feat: aula sobre variáveis

---
+++ introducao-programacao-computadores/aula2-variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int idade;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = "Sergio";
+
+    idade = 25;
+    quantidade = 10;
+    
+    return 0;
+}
