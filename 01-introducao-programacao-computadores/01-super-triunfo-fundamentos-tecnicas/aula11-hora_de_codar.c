a79677b61d68399f9973f258430b0e39bc25e751
renancvitor <renan.vitor.cm@gmail.com>
2025-08-07 16:57:44 -0300

    feat: adiciona esqueleto aula 11 - hora de codar

---
+++ 01-introducao-programacao-computadores/01-super-triunfo-fundamentos-tecnicas/aula11-hora_de_codar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string>
+
+// Variáveis
+char produtoA[50] = "Produto A";
+char produtoB[50] = "Produto B";
+
+unsigned int estoqueProdutoA = 1000;
+unsigned int estoqueProdutoB = 2000;
+
+float valorProdutoA = 10.50;
+float valorProdutoB = 20.40;
+
+unsigned int estoqueMinimoProdutoA = 500;
+unsigned int estoqueMinimoProdutoB = 2500;
+
+double valorTotalProdutoA;
+double valorTotalProdutoB;
+
+// Funções
+void entradadaDados() {
+}
+
+double calculaValorTotalProdutoA() {
+}
+
+double calculaValorTotalProdutoA() {
+}
+
+void saidaDados() {
+}
+
+int main () {
+
+    return 0;
+}
