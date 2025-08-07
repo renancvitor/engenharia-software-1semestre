4ba911c6d6de3bdfddb67c52b9d4d19cd8e3bd5f
renancvitor <renan.vitor.cm@gmail.com>
2025-08-07 18:04:31 -0300

    feat: adiciona resolução código aula 11 - hora de codar

--- 01-introducao-programacao-computadores/01-super-triunfo-fundamentos-tecnicas/aula11-hora_de_codar.c
+++ 01-introducao-programacao-computadores/01-super-triunfo-fundamentos-tecnicas/aula11-hora_de_codar.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <string>
+#include <string.h>
 
-// Variáveis
 char produtoA[50] = "Produto A";
 char produtoB[50] = "Produto B";
 
@@ -17,20 +16,64 @@ unsigned int estoqueMinimoProdutoB = 2500;
 double valorTotalProdutoA;
 double valorTotalProdutoB;
 
-// Funções
-void entradadaDados() {
+int resultadoA, resultadoB;
+
+double calculaValorTotalProdutoA()
+{
+    return (double)valorProdutoA * estoqueProdutoA;
 }
 
-double calculaValorTotalProdutoA() {
+double calculaValorTotalProdutoB()
+{
+    return (double)valorProdutoB * estoqueProdutoB;
 }
 
-double calculaValorTotalProdutoA() {
+bool comparaProdutoA()
+{
+    return estoqueProdutoA > estoqueMinimoProdutoA;
 }
 
-void saidaDados() {
+bool comparaProdutoB()
+{
+    return estoqueProdutoB > estoqueMinimoProdutoB;
 }
 
-int main () {
+bool comparaValorProdutos()
+{
+    valorTotalProdutoA = calculaValorTotalProdutoA();
+    valorTotalProdutoB = calculaValorTotalProdutoB();
 
+    if (calculaValorTotalProdutoA() > calculaValorTotalProdutoB())
+    {
+        printf("\nO valor total do produto A (R$ %.2f) é maior do que o valor total de produto B (R$ %.2f).\n", valorTotalProdutoA, valorTotalProdutoB);
+        return true;
+    }
+    else
+    {
+        printf("\nO valor total do produto B (R$ %.2f) é maior do que o valor total de produto A (R$ %.2f).\n", valorTotalProdutoB, valorTotalProdutoA);
+        return false;
+    }
+}
+
+void saidaDados()
+{
+    printf("Produto A %s tem estoque %u e o valor unitário é R$ %.2f. Valor total em estoque R$ %.2f\n", produtoA, estoqueProdutoA, valorProdutoA, valorTotalProdutoA);
+    printf("O produto %s tem estoque mínimo %d\n", produtoA, resultadoA);
+
+    printf("\nProduto B %s tem estoque %u e o valor unitário é R$ %.2f. Valor total em estoque R$ %.2f\n", produtoB, estoqueProdutoB, valorProdutoB, valorTotalProdutoB);
+    printf("O produto %s tem estoque mínimo %d\n", produtoB, resultadoB);
+}
+
+int main()
+{
+    valorTotalProdutoA = calculaValorTotalProdutoA();
+    valorTotalProdutoB = calculaValorTotalProdutoB();
+
+    resultadoA = comparaProdutoA();
+    resultadoB = comparaProdutoB();
+
+    saidaDados();
+
+    comparaValorProdutos();
     return 0;
 }
