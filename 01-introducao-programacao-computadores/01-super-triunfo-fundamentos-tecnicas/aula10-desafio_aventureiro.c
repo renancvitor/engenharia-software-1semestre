20e77150976c2f851fd00c27ed98004fa0de4875
renancvitor <renan.vitor.cm@gmail.com>
2025-08-07 15:00:45 -0300

    feat: adiciona arquivo aula 10 - desafio aventureiro

---
+++ 01-introducao-programacao-computadores/01-super-triunfo-fundamentos-tecnicas/aula10-desafio_aventureiro.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <string.h>
+
+char estado, estado2;
+char codigoCarta[50], nomeCidade[50], codigoCarta2[50], nomeCidade2[50];
+unsigned long int populacao, populacao2;
+int numeroPontosTuristicos, numeroPontosTuristicos2;
+double area, pib, area2, pib2, pibPerCapta, pibPerCapta2, densidadePopulacional, densidadePopulacional2;
+
+char estadoStr[10], populacaoStr[20], areaStr[20], pibStr[20], pontosStr[20];
+char estadoStr2[10], populacaoStr2[20], areaStr2[20], pibStr2[20], pontosStr2[20];
+
+void readLine(char *buffer, int size)
+{
+    if (fgets(buffer, size, stdin))
+    {
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
+}
+
+void entradaDados() {
+    printf("\nDigite abaixo os dados da primeira carta:");
+
+    printf("\nEstado: ");
+    readLine(estadoStr, sizeof(estadoStr));
+    estado = estadoStr[0];
+
+    printf("Código: ");
+    readLine(codigoCarta, sizeof(codigoCarta));
+
+    printf("Nome da Cidade: ");
+    readLine(nomeCidade, sizeof(nomeCidade));
+
+    printf("População: ");
+    readLine(populacaoStr, sizeof(populacaoStr));
+    sscanf(populacaoStr, "%lu", &populacao);
+
+    printf("Área: ");
+    readLine(areaStr, sizeof(areaStr));
+    sscanf(areaStr, "%lf", &area);
+
+    printf("PIB: ");
+    readLine(pibStr, sizeof(pibStr));
+    sscanf(pibStr, "%lf", &pib);
+
+    printf("Número de Pontos Turísticos: ");
+    readLine(pontosStr, sizeof(pontosStr));
+    sscanf(pontosStr, "%d", &numeroPontosTuristicos);
+
+    printf("\nDigite abaixo os dados da segunda carta:");
+
+    printf("\nEstado: ");
+    readLine(estadoStr2, sizeof(estadoStr2));
+    estado2 = estadoStr2[0];
+
+    printf("Código: ");
+    readLine(codigoCarta2, sizeof(codigoCarta2));
+
+    printf("Nome da Cidade: ");
+    readLine(nomeCidade2, sizeof(nomeCidade2));
+
+    printf("População: ");
+    readLine(populacaoStr2, sizeof(populacaoStr2));
+    sscanf(populacaoStr2, "%lu", &populacao2);
+
+    printf("Área: ");
+    readLine(areaStr2, sizeof(areaStr2));
+    sscanf(areaStr2, "%lf", &area2);
+
+    printf("PIB: ");
+    readLine(pibStr2, sizeof(pibStr2));
+    sscanf(pibStr2, "%lf", &pib2);
+
+    printf("Número de Pontos Turísticos: ");
+    readLine(pontosStr2, sizeof(pontosStr2));
+    sscanf(pontosStr2, "%d", &numeroPontosTuristicos2);
+
+    printf("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
+}
+
+double calcularDensidadePopulacional() {
+    return (double) populacao / area;
+}
+
+double calcularPibPerCapta() {
+    return (pib * 1e9) / (double) populacao;
+}
+
+double calcularDensidadePopulacional2() {
+    return (double) populacao2 / area2;
+}
+
+double calcularPibPerCapta2() {
+    return (pib2 * 1e9) / (double) populacao2;
+}
+
+void exibeCalculosCarta1() {
+    densidadePopulacional = calcularDensidadePopulacional();
+    pibPerCapta = calcularPibPerCapta();
+
+    printf("Densidade Populacional:  %.2lf hab/km²", densidadePopulacional);
+    printf("\nPIB per Capita:  %.2lf reais", pibPerCapta);
+    printf("\n");
+}
+
+void exibeCalculosCarta2() {
+    densidadePopulacional2 = calcularDensidadePopulacional2();
+    pibPerCapta2 = calcularPibPerCapta2();
+
+    
+    printf("Densidade Populacional:  %.2lf hab/km²", densidadePopulacional2);
+    printf("\nPIB per Capita:  %.2lf reais", pibPerCapta2);
+    printf("\n");
+}
+
+void saidaDados() {
+    printf("\nPrimeira carta:");
+    printf("\nEstado: %c", estado);
+    printf("\nCódigo da Carta: %s", codigoCarta);
+    printf("\nNome da Cidade: %s", nomeCidade);
+    printf("\nPopulação: %lu", populacao);
+    printf("\nÁrea: %.2lf km²", area);
+    printf("\nPIB: %.2lf bilhões de reais", pib);
+    printf("\nNúmero de Pontos Turísticos: %d\n", numeroPontosTuristicos);
+    exibeCalculosCarta1();
+    
+    printf("\nSegunda carta:");
+    printf("\nEstado: %c", estado2);
+    printf("\nCódigo da Carta: %s", codigoCarta2);
+    printf("\nNome da Cidade: %s", nomeCidade2);
+    printf("\nPopulação: %lu", populacao2);
+    printf("\nÁrea: %.2lf km²", area2);
+    printf("\nPIB: %.2lf bilhões de reais", pib2);
+    printf("\nNúmero de Pontos Turísticos: %d\n", numeroPontosTuristicos2);
+    exibeCalculosCarta2();
+}
+
+int main()
+{
+    printf("\n***Super Trunfo - Desafio aventureiro***\n");
+
+    entradaDados();
+    saidaDados();
+
+    return 0;
+}
