e98e32a96e7d56e8afe367a5e0c1fd6d7c7a1650
renancvitor <renan.vitor.cm@gmail.com>
2025-08-08 12:34:07 -0300

    feat: adiciona esquele inicial aula 01

---
+++ 01-introducao-programacao-computadores/02-super-trunfo-desenvolvendo-logica/aula1-estrutura_decisao_simples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero1 = 10, numero2 = 5;
+
+    if (numero1 > numero2)
+    {
+        printf("Número 1 é maior do que número 2");
+    }
+
+    printf("\nFora do if");
+    // minuto 5:30
+    return 0;
+}
